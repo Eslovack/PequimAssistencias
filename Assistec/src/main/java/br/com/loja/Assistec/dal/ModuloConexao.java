6614a1e06d05572e17f4ae75352eaa85abecdb69
Carlos Eduardo de Souza Furtunato <carlos.esf@aluno.ifsc.edu.br>
2024-09-18 10:39:37 -0300

    Projeto 1

---
+++ Assistec/src/main/java/br/com/loja/Assistec/dal/ModuloConexao.java
@@ -0,0 +1,27 @@
+package br.com.loja.Assistec.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ModuloConexao {
+	
+	public static Connection conector() {
+		
+		Connection conexao = null;
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/lojadb";
+		String user = "root";
+		String password = "aluno";
+		
+		try {
+			
+			Class.forName(driver);
+			conexao = DriverManager.getConnection(url, user, password);
+			return conexao;
+	} catch (Exception e) {
+			return null;
+	}
+		
+	
+  }
+}
---
+++ Assistec/src/test/java/br/com/loja/Assistec/AppTest.java
@@ -0,0 +1,16 @@
+package br.com.loja.Assistec;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {		
+
+    /**
+     * Rigorous Test :-)
+     */
+    
+    public void shouldAnswerWithTrue() {
+        
+    }
+}
